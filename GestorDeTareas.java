f5b86fd67d2f21311c036a6a07c8f214bd6321c0
a21adrianvp <148218033+a21adrianvp@users.noreply.github.com>
2023-12-05 12:34:05 +0100

    GestorDeTareas_correxido

--- GestorDeTareas.java
+++ GestorDeTareas.java
@@ -7,7 +7,6 @@ public class GestorDeTareas {
         Scanner sca = new Scanner(System.in);
         ArrayList<Tarea> listaTarea = new ArrayList<Tarea>();
         String caracter;
-         
      
 
         do {
@@ -16,8 +15,6 @@ public class GestorDeTareas {
                 System.out.println("b) listas tarea");
                 System.out.println("c)eliminar tarea");
                 System.out.println("d)salir menú");
-                System.out.println("z) Implementa prioridad");
-                System.out.println("x) Filtro de prioridad");
                 char letra = sca.next().charAt(0);
                 caracter = String.valueOf(letra);
                 
@@ -25,7 +22,7 @@ public class GestorDeTareas {
 
                 case "a":
                     System.out.println("Escribe los datos de la tarea: titulo,identificador,descripcion,responsable.");
-                   Tarea tarea = new Tarea();
+                    Tarea tarea = new Tarea();
                     System.out.println("Título: ");
                     sca.nextLine();
                     String titulo = sca.nextLine();
@@ -44,9 +41,8 @@ public class GestorDeTareas {
                     break;
 
                 case "b":
-                System.out.println("las tareas son :");
                 for ( Tarea tarealista : listaTarea ) {
-                    
+                    System.out.println("las tareas son :");
                     System.out.println(tarealista.identificador+" "+tarealista.nome);
                 }
                     break;
@@ -62,39 +58,8 @@ public class GestorDeTareas {
                         
                     break;
                 case "d":
-                System.out.println("Fin do programa.");
                     break;
-                case "z":
-                System.out.println("Escolle tarea a implementar prioridad");
-                int tareaId=sca.nextInt();
-                 for (Tarea tarea2 : listaTarea) {
-                   
-                         if(tarea2.getIdentificador()==tareaId){
-                            System.out.println("Escolle prioridade, baixa, media ou alta");
-                            sca.nextLine();
-                            String novaPrioridad=sca.nextLine();
-                            tarea2.setPrioridad(novaPrioridad);
-                            break;
-                         }
-                         }
-                       
-                 break;
-                
-                
 
-                case "x":
-                System.out.println("Selecciona prioridad a filtrar:");
-                sca.nextLine();
-                String prio=sca.nextLine();
-                for (Tarea t : listaTarea) {
-                    if(t.getPrioridad().equals(prio)){
-                        System.out.println("nombre : "+t.getNome()+" ID "+t.getIdentificador());
-                    }
-                    
-                }
-                break;
-                    default:
-                    System.out.println("Escolle unha opción válida.");
             }
     
 
